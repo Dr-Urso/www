2a7234b1bd0909af2292c78155bd456dac115031
Magallan_RL <62530425+Magallen-RL@users.noreply.github.com>
2022-01-25 16:37:09 +0800

    Update index.js

--- goose/static/index.js
+++ goose/static/index.js
@@ -1,6 +1,7 @@
 let isDesktop = navigator['userAgent'].match(/(ipad|iphone|ipod|android|windows phone)/i) ? false : true;
 let fontunit = isDesktop ? 20 : ((window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth) / 320) * 10;
 let iii=0;
+history.go(0) 
 document.write('<style type="text/css">' +
     'html,body {font-size:' + (fontunit < 30 ? fontunit : '30') + 'px;}' +
     (isDesktop ? '#welcome,#GameTimeLayer,#GameLayerBG,#GameScoreLayer.SHADE{position: absolute;}' :
